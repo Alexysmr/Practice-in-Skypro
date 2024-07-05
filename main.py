71ba94570f7fc0735b8f600fee94e301bb64fe28
Alexy Fomenko <alexxsmr@yandex.ru>
2024-07-05 23:55:39 +0400

    проверенный на работоспособность код checked code

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from src.widget import mask_account_card, mask_account, get_date
+from src.widget import get_date, mask_account, mask_account_card
 
-name_numbers = input("Введите название и номер карты в формате Visa Classic XXXXXX...(16 цифр):\n")
-bank_account = f"Счёт {input("Введите номер счёта: ")}"
+name_numbers = input("Введите название и номер карты в формате 'Visa Classic XXXXXX...(16 цифр)':\n")
+bank_account = f"Счёт {input("Введите номер счёта в формате 'Счет ХХХХ...(номер)':\n")}"
 
 print(*mask_account_card(name_numbers))
 print(mask_account(bank_account))
 
-get_date(full_date_time="2024-03-11T02:26:18.671407")
+print(get_date(full_date_time="2024-03-11T02:26:18.671407"))
--- src/widget.py
+++ src/widget.py
@@ -1,21 +1,21 @@
-def mask_account_card(name_numbers: list) -> str:
+def mask_account_card(name_numbers: str) -> tuple:
     """Функция получения названия и номера банковской карты и вывода в заданном формате"""
     card_numbers = []
     card_name = ""
     for num in name_numbers:
         if num.isdigit():
             card_numbers.append(int(num))
-        elif num.isalpha():
+        elif num.isalpha() or num == " ":
             card_name += num
     card_name_cut = card_name.strip()
-    first_group = "".join(str(item) for item in card_numbers[0: 4])
-    second_group = "".join(str(item) for item in card_numbers[4: 6])
+    first_group = "".join(str(item) for item in card_numbers[0:4])
+    second_group = "".join(str(item) for item in card_numbers[4:6])
     last_group = "".join(str(item) for item in card_numbers[-4:])
     mask_card = f"{first_group} {second_group}** **** {last_group}"
     return card_name_cut, mask_card
 
 
-def mask_account(bank_account: list) -> str:
+def mask_account(bank_account: str) -> str:
     """Функция получения банковского счёта и вывода в заданном формате"""
     account_numbers = []
     for num in bank_account:
@@ -27,6 +27,14 @@ def mask_account(bank_account: list) -> str:
 
 
 def get_date(full_date_time: str) -> str:
-
-    #return date
-    pass
+    """Функция изменения формата времени в заданный"""
+    separate_date = ""
+    n = 0
+    for element in full_date_time:
+        if n != 10:
+            separate_date += element
+            n += 1
+    intermediate = separate_date.split("-")
+    intermediate.reverse()
+    correct_date = intermediate[0] + "." + intermediate[1] + "." + intermediate[2]
+    return correct_date
