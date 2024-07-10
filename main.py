a12a0f9be2e3269b7e34d7d326d8e4a194085794
Alexy Fomenko <alexxsmr@yandex.ru>
2024-07-10 22:54:15 +0400

    update main.py, processing.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from src.widget import get_date, mask_account, mask_account_card
+from src.processing import filter_by_state, sort_by_date
 
-name_numbers = input("Введите название и номер карты в формате 'Visa Classic XXXXXX...(16 цифр)':\n")
-bank_account = f"Счёт {input("Введите номер счёта в формате 'Счет ХХХХ...(номер)':\n")}"
+clients = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+           {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+           {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+           {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
 
-print(*mask_account_card(name_numbers))
-print(mask_account(bank_account))
 
-print(get_date(full_date_time="2024-03-11T02:26:18.671407"))
+print(filter_by_state(clients))
+print(filter_by_state(clients, state='CANCELED'))
+print(sort_by_date(clients))
--- src/processing.py
+++ src/processing.py
@@ -1,6 +1,13 @@
-def filter_by_state(clients: list, state='EXECUTED') -> list:
-    pass
+def filter_by_state(clients: list, state: str ='EXECUTED') -> list:
+    """Функция фильтрации по статусу state"""
+    filtered = []
+    for client in clients:
+        if client['state'] == state:
+            filtered.append(client)
+    return filtered
 
 
-def sort_by_date(clients: list, sort=reverse) ->list:
-    pass
+def sort_by_date(clients: list, sort_reverse: bool=True) ->list:
+    """Функция сортировки по дате"""
+    date_sorted = sorted(clients, key=lambda clients: clients['date'], reverse=sort_reverse)
+    return date_sorted
