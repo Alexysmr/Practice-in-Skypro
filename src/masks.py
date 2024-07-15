38d5a7f045b35820c3b091a8573c939e6277bcd3
Alexy Fomenko <alexxsmr@yandex.ru>
2024-07-16 00:03:16 +0400

    modified main.py, add and modified masks.py

---
+++ src/masks.py
@@ -0,0 +1,34 @@
+def get_mask_card_number(numbers: list) -> str:
+    """Функция получения номера банковской карты и вывода в заданном формате"""
+    card_numbers = []
+    if len(numbers) == 16:
+        for num in numbers:
+            if num.isdigit():
+                card_numbers.append(int(num))
+            else:
+                return "Ошибка номера карты"
+    else:
+        return "Ошибка номера карты"
+
+    first_group = "".join(str(item) for item in card_numbers[0:4])
+    second_group = "".join(str(item) for item in card_numbers[4:6])
+    last_group = "".join(str(item) for item in card_numbers[12:])
+    mask_number = f"{first_group} {second_group}** **** {last_group}"
+    return mask_number
+
+
+def get_mask_account(bank_account: list) -> str:
+    """Функция получения банковского счёта и вывода в заданном формате"""
+    account_numbers = []
+    if len(bank_account) == 20:
+        for num in bank_account:
+            if num.isdigit():
+                account_numbers.append(int(num))
+            else:
+                return "Ошибка номера счёта"
+    else:
+        return "Ошибка номера счёта"
+
+    mask = "".join(str(item) for item in account_numbers[0:])
+    mask_account = f"**{mask[-4:]}"
+    return mask_account
