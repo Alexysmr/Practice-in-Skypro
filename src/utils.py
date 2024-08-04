01737c48711a47ba9c10faace9314138e0dc8e9e
Alexy Fomenko <alexxsmr@yandex.ru>
2024-08-04 23:19:55 +0400

    update src.utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,18 +1,19 @@
 import json
 import requests
-import os.path
 import os
+import os.path
 from pathlib import Path
+from typing import Any
 from dotenv import load_dotenv
 
 
-def input_transactions(filename) -> list:
+def input_transactions(filename: str) -> list[dict[Any, Any]]:
     """Функция принимает на вход путь до JSON-файла и
     возвращает список словарей с данными о финансовых транзакциях"""
     main_path = Path(__file__).resolve().parents[1]
     operations_file = os.path.join(main_path, "data", filename)
     if os.path.exists(operations_file) and os.stat(operations_file).st_size != 0:
-        with open(operations_file, encoding='utf-8') as f:
+        with open(operations_file, encoding="utf-8") as f:
             transactions_data = json.load(f)
         if isinstance(transactions_data, list) and len(transactions_data) != 0:
             return transactions_data
@@ -22,17 +23,17 @@ def input_transactions(filename) -> list:
         return []
 
 
-def currency_exchange(transactions_data: dict) -> list:
+def currency_exchange(transactions_data: dict[Any, Any]) -> list:
     """Функция принимает на вход транзакцию и возвращает сумму транзакции
     (amount) в рублях, тип данных — float. Для пересчета из USD или EUR
     в рубли происходит обращение к внешнему API"""
     amount_list = []
     if len(transactions_data) != 0:
         main_path = Path(__file__).resolve().parents[1]
-        dotenv_path = os.path.join(main_path, '.apisett.env')
+        dotenv_path = os.path.join(main_path, ".apisett.env")
         if os.path.exists(dotenv_path):
             load_dotenv(dotenv_path)
-        api_token = os.getenv('API_KEY')
+        api_token = os.getenv("API_KEY")
         url_eur = "https://api.apilayer.com/exchangerates_data/convert?to=RUB&from=EUR&amount=1"
         url_usd = "https://api.apilayer.com/exchangerates_data/convert?to=RUB&from=USD&amount=1"
         payload_eur = {}
@@ -42,22 +43,22 @@ def currency_exchange(transactions_data: dict) -> list:
         status_code_eur = response_eur.status_code
         if status_code_eur < 400:
             result_eur = response_eur.json()
-            eur_course = result_eur.get("info", "").get("rate", "")
+            eur_course = result_eur.get("result", "")
         else:
             eur_course = ["error"]
         response_usd = requests.request("GET", url_usd, headers=headers, data=payload_usd)
         status_code_usd = response_usd.status_code
         if status_code_usd < 400:
             result_usd = response_usd.json()
-            usd_course = result_usd.get("info", "").get("rate", "")
+            usd_course = result_usd.get("result", "")
         else:
             usd_course = ["error"]
 
         if isinstance(usd_course, float) and isinstance(eur_course, float):
             for x in transactions_data:
                 try:
-                    amount = float(x.get('operationAmount', "").get('amount', ""))
-                    currency_code = x.get('operationAmount', "").get('currency', "").get('code', "")
+                    amount = float(x.get("operationAmount", "").get("amount", ""))
+                    currency_code = x.get("operationAmount", "").get("currency", "").get("code", "")
                     # id_number = x.get("id")
                     if currency_code == "USD":
                         amount = round(float(amount * usd_course), 2)
